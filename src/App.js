0ca5d75f5910a35debc96c339835cf876a172126
Peregud Oxana <ox.peregud@gmail.com>
2022-03-28 10:50:53 +0300

    feat: added users list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import {useState} from "react";
 
 function App() {
 
+    const [usersList, setUsersList] = useState([]);
+
+    const addUserHandler = (username, age) => {
+        setUsersList((prevUsersList) => {
+            return [...prevUsersList, {
+                name: username,
+                age: age,
+                id: Math.random().toString()
+            }];
+        });
+    };
+
     return (
         <div>
-            <AddUser/>
+            <AddUser onAddUser={addUserHandler}/>
+            <UsersList users={usersList}/>
         </div>
     );
 }
