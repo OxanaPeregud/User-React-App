1696ac0b9e622b95f27de9ee631d900b7a8ba42c
Peregud Oxana <ox.peregud@gmail.com>
2022-03-29 11:56:55 +0300

    feat: added new portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,12 +1,18 @@
 import styles from './ErrorModal.module.css';
 import Card from "./Card";
 import Button from "./Button";
+import ReactDOM from 'react-dom';
 
 const ErrorModal = (props) => {
 
-    return (
-        <>
+    const Backdrop = (props) => {
+        return (
             <div className={styles.backdrop} onClick={props.onConfirm}/>
+        );
+    };
+
+    const ModalOverlay = (props) => {
+        return (
             <Card className={styles.modal}>
                 <header className={styles.header}>
                     <h2>{props.title}</h2>
@@ -18,6 +24,18 @@ const ErrorModal = (props) => {
                     <Button onClick={props.onConfirm}>Okay</Button>
                 </footer>
             </Card>
+        );
+    };
+
+    return (
+        <>
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/>,
+                document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onConfirm={props.onConfirm}/>,
+                document.getElementById('overlay-root'))}
         </>
     );
 }
